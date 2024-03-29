ed5ba68069373173e31d9b8c1a010d1df192e752
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2024-03-29 20:05:07 +0100

    refactored

--- apps/rag_app.py
+++ apps/rag_app.py
@@ -9,16 +9,13 @@ class App(Tk):
         super().__init__()
         self.geometry("700x550")
         self.title("Rag App")
-        
-        
-        self.rag_text = Text(self, width = 80, height = 20)
-        self.rag_text.pack(pady=20)
-        
-        self.load_pdf = Button(self, text = "Get PDF", command = self.get_pdf)
-        self.load_pdf.pack(pady=20)
+        self.text_box()
+        self.add_load_pdf_button()
         self.my_file = ""
         self.rag = None
-        
+        self.add_search_panel()
+
+    def add_search_panel(self):
         self.search_entry = Entry(self)
         self.search_entry.insert(0, "Search query")
         self.search_entry.config(state=DISABLED)
@@ -27,16 +24,20 @@ class App(Tk):
         self.search_button = Button(self, text="Search", command=self.search)
         self.search_entry.pack(side=LEFT, padx=5, expand=True, fill=X)
         self.search_button.pack(side=LEFT, padx=5)
-        
-        
-        
+
+    def add_load_pdf_button(self):
+        self.load_pdf_button = Button(self, text="Get PDF", command=self.get_pdf)
+        self.load_pdf_button.pack(pady=20)
+
+    def text_box(self):
+        self.rag_text = Text(self, width=80, height=20)
+        self.rag_text.pack(pady=20)
+
     def get_pdf(self):
         self.my_file = filedialog.askopenfilename(initialdir = "", title = "Select PDF", filetypes = (("PDF files", "*.pdf"), ("all files", "*.*")))
         if self.my_file:
             self.rag_text.insert(END, self.my_file)
-            
-            
-            
+
     def search(self):
         self.rag_text.insert(END, "You are looking for: " + self.search_entry.get())
         if self.my_file == "":
@@ -53,11 +54,11 @@ class App(Tk):
             text = self.rag.display_results(results)
 
             self.rag_text.insert(END, text)
-            
-        
+
     def click(self, event):
         self.search_entry.config(state=NORMAL)
         self.search_entry.delete(0, END)
+
 
 if __name__ == "__main__":
     app = App()
