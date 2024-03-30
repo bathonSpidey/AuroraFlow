63caa70868b3330656d7ba66cd693bfca96a6614
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2024-03-30 22:30:26 +0100

    multi rag app and readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# AuroraFlow
+# AuroraFlow 
+Aurora flow is basically an easy library for making custom LLMs using RAG. This repository consists of the
+rag libraries and the custom LLMs using Hugging face. Currently the rag part of the project is finished.
+
+## Aurora Rag
  A tool for RAG pipeline and easy processing of files. 
  The idea behind this is that one can feed in files in form of pdf such as scientific papers, books etc.
 and the system gives you the most relevant answers it found in all these papers along with the page number and file it found it in. 
@@ -21,4 +25,13 @@ The demo below shows how aurora flow helps one to extract relevant information f
 
 
 ![Flow Demo](auroraflowdemo.gif)
+
+## Multi Aurora Rag
+THis app helps to group multiple sources books, research papers together and then search for the material. 
+
+The running and installation is similar instead of step 3 you would do
+````commandline
+python -m apps.multi_rag_app
+````
+If you would want to pair program and contribute let me know would be happy to collab
 
---
+++ apps/multi_rag_app.py
@@ -0,0 +1,67 @@
+from tkinter import *
+from tkinter import filedialog
+
+from src.MultiRag.MultiAuroraRag import MultiAuroraRag
+from src.Rag.AuroraRag import AuroraRag
+
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("700x550")
+        self.title("Rag App")
+        self.text_box()
+        self.add_load_pdf_button()
+        self.my_file = ""
+        self.rag = None
+        self.add_search_panel()
+
+    def add_search_panel(self):
+        self.search_entry = Entry(self)
+        self.search_entry.insert(0, "Search query")
+        self.search_entry.config(state=DISABLED)
+        self.search_entry.bind("<Button-1>", self.click)
+        self.search_entry.bind("<Return>", lambda event: self.search())
+        self.search_button = Button(self, text="Search", command=self.search)
+        self.search_entry.pack(side=LEFT, padx=5, expand=True, fill=X)
+        self.search_button.pack(side=LEFT, padx=5)
+
+    def add_load_pdf_button(self):
+        self.load_pdf_button = Button(self, text="Open Folder", command=self.get_pdf)
+        self.load_pdf_button.pack(pady=20)
+
+    def text_box(self):
+        self.rag_text = Text(self, width=80, height=20)
+        self.rag_text.pack(pady=20)
+
+    def get_pdf(self):
+        self.my_file = filedialog.askdirectory()
+        if self.my_file:
+            self.rag_text.insert(END, self.my_file)
+
+    def search(self):
+        self.rag_text.insert(END, "You are looking for: " + self.search_entry.get())
+        if self.my_file == "":
+            self.rag_text.insert(END, "No file found, make sure that the file exists and readable.")
+        else:
+            if self.search_entry.get() == "":
+                self.rag_text.insert(END, "Please enter a search query")
+                return
+            self.rag = MultiAuroraRag(self.my_file, "all-mpnet-base-v2")
+            self.rag_text.insert(END, "creating rag pipeline to search your file...")
+
+            results = self.rag.search(self.search_entry.get())
+            self.rag_text.delete("1.0", END)
+            text = self.rag.display_results(results)
+
+            self.rag_text.insert(END, text)
+
+    def click(self, event):
+        self.search_entry.config(state=NORMAL)
+        self.search_entry.delete(0, END)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
